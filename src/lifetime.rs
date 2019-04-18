71c9744eabaac28fc8a45b70ed47c37f4f3b156b
anymost <creatuture@gmail.com>
2019-04-18 10:19:21 +0800

    add lifetime

--- src/lifetime.rs
+++ src/lifetime.rs
@@ -1,7 +1,16 @@
 // 引用的生命周期，主要涉及到引用
 // 借助借用检查器，rust认为一个变量引用的另外一个变量的生命周期必须比自己长，不然就报错。
 // 生命周期注解，不会修改引用的生命周期，而是指出引用的生命周期
-// &str的生命周期是全局的
+// &str属于静态生命周期，也就是全局的
+/**
+ * 生命周期省略规则
+ * 参数的生命周期为输入生命周期，返回值的生命周期为输出生命周期
+ * 1 每一个为引用的参数都有一个生命周期参数
+ * 2 如果只有一个输入生命周期参数，那么它被赋予所有的输出生命周期参数
+ * 3 如果有多个输入生命周期参数，其中一个为&self或&mut self,那么它被赋予所有的输出生命周期参数
+ * 
+ * 借助上面的三条规则，来判断是否能确定一个函数中的所有生命周期参数，如果无法确定，那么就编译失败
+ */
 
 
 
@@ -30,6 +39,24 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
 }
 
 
+
+#[derive(Debug)]
+struct User<'a> {
+    name: &'a str,
+    age: i32
+}
+
+fn origin(a: & str) -> & str {
+    a
+}
+
+impl<'a> User<'a> {
+    fn say_name(&self, v: &str) -> &str {
+        self.name
+    }
+}
+
+
 fn main() {
     // let a = "hello";
     // let b = "world";
@@ -50,4 +77,16 @@ fn main() {
     //     c = longest(a, b.as_str());
     // }
     // println!("{}", c);
+
+
+    let s = String::from("hello");
+    let s2 = String::from("world");
+   
+    let user = User{
+        name: &s[..],
+        age: 20
+    };
+
+    let val = user.say_name(&s2);
+    println!("{}", val);
 }
