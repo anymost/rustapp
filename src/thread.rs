e0b0263d7a182345912619f2a54260d061032dbf
anymost <creatuture@gmail.com>
2019-04-28 20:08:05 +0800

    add thread and channel

---
+++ src/thread.rs
@@ -0,0 +1,50 @@
+// 运行时：指二进制文件中由语言本身提供的代码
+// 使用join,阻塞主线程，等待子线程执行完成
+
+
+use std::thread;
+use std::time::Duration;
+
+fn base() {
+    thread::spawn(||{
+        for i in 1..10 {
+            println!("other {}", i);
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for i in 1..10 {
+        println!("main {}", i);
+        thread::sleep(Duration::from_secs(1));
+    }
+}
+
+fn join() {
+    let handle = thread::spawn(||{
+        for i in 0..20{
+            println!("other {}", i);
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    handle.join().unwrap_err();
+
+    for i in 0..10{
+        println!("main {}", i);
+        thread::sleep(Duration::from_secs(1));
+    }
+}
+
+fn move_thread() {
+    let vector = vec![1, 2, 3, 4, 5];
+    let handle = thread::spawn(move ||{
+        println!("{:?}", vector);
+    });
+    handle.join().unwrap();
+}
+
+fn main() {
+    // base();
+    // join();
+    move_thread();
+}
